7f4bd0f5b083451aaa694e046591ddf5a659980f
Ethan <ethankone81@gmail.com>
2018-11-15 14:41:02 -0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -3,10 +3,21 @@ TITLE = "jumpy"
 WIDTH = 480
 HEIGHT = 600
 # frames per second
-FPS = 30
+FPS = 60
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 REDDISH = (240,55,66)
-
+FONT_NAME = 'arial'
+# player settings
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
+# platform settings
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (65, HEIGHT - 300, WIDTH-400, 40),
+                 (20, HEIGHT - 350, WIDTH-300, 40),
+                 (200, HEIGHT - 150, WIDTH-350, 40),
+                 (200, HEIGHT - 450, WIDTH-350, 40)]
 
--- sprites.py
+++ sprites.py
@@ -1,79 +1,67 @@
 # sprite classes for game
 # i used some ideas from CodePylet https://www.youtube.com/watch?v=osDofIdja6s&t=1038s
 # i also borrowed pretty much all of this from kids can code - thanks!
+# on acceleration https://www.khanacademy.org/science/physics/one-dimensional-motion/kinematic-formulas/v/average-velocity-for-constant-acceleration 
+# on vectors: https://www.youtube.com/watch?v=ml4NSzCQobk 
+
 
 import pygame as pg
 from pygame.sprite import Sprite
 import random
 from settings import *
 
+vec = pg.math.Vector2
+
 class Player(Sprite):
-    def __init__(self):
+    def __init__(self, game):
         Sprite.__init__(self)
+        self.game = game
         self.image = pg.Surface((30,40))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT /2)
-        self.vx = 0
-        self.vy = 0
-        self.falling = False
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        print("adding vecs " + str(self.vel + self.acc))
+
     def update(self):
-        self.vx = 0
-        # self.vy = 0
-        self.gravity()
+        self.acc = vec(0, PLAYER_GRAV)
+        print("acc " + str(self.acc))
+        print("vel " + str(self.vel))
+
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
-            self.vx = -5
+            self.acc.x =  -PLAYER_ACC
         if keys[pg.K_RIGHT]:
-            self.vx = 5
-        if keys[pg.K_UP] and self.falling == False:
-            self.jump()
-        self.rect.x += self.vx
-        self.rect.y += self.vy
-    def gravity(self):
-        if self.rect.y < HEIGHT-40:
-            self.falling = True
-            print("gravity is happening! " + str(self.rect.y))
-            print("falling " + str(self.falling))
-            self.vy += 10
-        elif self.rect.y >= HEIGHT:
-            self.falling = False
-            self.vy = 0
-            self.rect.y = HEIGHT-40
-            print("gravity is NOT happening! " + str(self.rect.y))
-            print("falling " + str(self.falling))
+            self.acc.x = PLAYER_ACC
+        # set player friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        # jump to other side of screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+    
     def jump(self):
-        self.vy = -75
-        print("jump called")
-
-
-class Enemy(Sprite):
-
-    def __init__(self):
-        Sprite.__init__(self)
-        self.image = pg.Surface((30,40))
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT /2)
-        self.vx = 0
-        self.vy = 0
-    def update(self):
-        self.vx = 0
-        keys = pg.key.get_pressed()
-        if keys[pg.K_a]:
-            self.vx = -5
-        if keys[pg.K_d]:
-            self.vx = 5
-
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        if hits:
+            self.vel.y = -20
 
 class Platform(Sprite):
-    def __init__(self):
+    def __init__(self, x, y, w, h):
         Sprite.__init__(self)
-        self.image = pg.Surface((400,20))
+        self.image = pg.Surface((w,h))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT /2)
+        self.rect.x = x
+        self.rect.y = y
 
 
